34294d04b44cd92fd10b853aebb9e6c998c1e4db
KpChuck <przestrzelski.com@gmail.com>
2017-09-16 22:30:24 +0100

    Tidy Qs Java

--- app/src/main/java/com/kpchuck/qstiles/RebootTileService.java
+++ app/src/main/java/com/kpchuck/qstiles/RebootTileService.java
@@ -7,30 +7,7 @@ package com.kpchuck.qstiles;
 public class RebootTileService extends android.service.quicksettings.TileService {
 
     public void onClick(){
-        reboot();
+
+        new ShellHelpOut().runShellCommands(new String[]{"reboot"});
     }
-
-
-
-    private void reboot() {
-
-        try {
-            ProcessBuilder builder = new ProcessBuilder( "su" );
-
-            Process p = builder.start();
-
-            //get stdin of shell
-            java.io.BufferedWriter p_stdin =
-                    new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));
-
-            p_stdin.write("reboot");
-            p_stdin.newLine();
-            p_stdin.flush();
-
-            p_stdin.write("exit");
-            p_stdin.newLine();
-            p_stdin.flush();
-        } catch (Exception e) {
-            android.util.Log.e("klock", "Error Rebooting", e);
-        }
-}}
+}
