c0859e2ea9ed596119cec264be126f86feebc85c
Prashant <marvelousprashant@gmail.com>
2017-09-17 02:05:11 +0530

    Add Power Off and Reboot QS Tiles

---
+++ app/src/main/java/com/kpchuck/qstiles/RebootTileService.java
@@ -0,0 +1,36 @@
+package com.kpchuck.qstiles;
+
+/**
+ * Created by Karol Przestrzelski on 05/09/2017.
+ */
+
+public class RebootTileService extends android.service.quicksettings.TileService {
+
+    public void onClick(){
+        reboot();
+    }
+
+
+
+    private void reboot() {
+
+        try {
+            ProcessBuilder builder = new ProcessBuilder( "su" );
+
+            Process p = builder.start();
+
+            //get stdin of shell
+            java.io.BufferedWriter p_stdin =
+                    new java.io.BufferedWriter(new java.io.OutputStreamWriter(p.getOutputStream()));
+
+            p_stdin.write("reboot");
+            p_stdin.newLine();
+            p_stdin.flush();
+
+            p_stdin.write("exit");
+            p_stdin.newLine();
+            p_stdin.flush();
+        } catch (Exception e) {
+            android.util.Log.e("klock", "Error Rebooting", e);
+        }
+}}
